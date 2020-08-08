cc774be6667f3607a4b07620133f19581768bcf1
Mohamed Haneef <mhaneef50673@gmail.com>
2020-08-08 20:02:26 +0400

    Add: modified save data logic to handle it in UI

--- src/utils.js
+++ src/utils.js
@@ -24,39 +24,8 @@ const readData = function (filePath) {
 };
 
 const saveData = function (dataToSave, filePath) {
-  const { playerOne, playerTwo, wonBy } = dataToSave;
-  const existingData = readData(filePath);
-  const { leaderboard: data } = existingData;
-  const existingPlayerOneIndex = lodash.findIndex(data, (val) =>
-    val.name === playerOne ? true : false
-  );
-  const existingPlayerTwoIndex = lodash.findIndex(data, (val) =>
-    val.name === playerTwo ? true : false
-  );
-  if (existingPlayerOneIndex > -1) {
-    const existingPlayerOneResult = data[existingPlayerOneIndex];
-    existingPlayerOneResult.points =
-      existingPlayerOneResult.points + (wonBy === playerOne ? 1 : 0);
-    data[existingPlayerOneIndex] = existingPlayerOneResult;
-  } else {
-    data.push({
-      name: playerOne,
-      points: wonBy === playerOne ? 1 : 0,
-    });
-  }
-  if (existingPlayerTwoIndex > -1) {
-    const existingPlayerTwoResult = data[existingPlayerTwoIndex];
-    existingPlayerTwoResult.points =
-      existingPlayerTwoResult.points + (wonBy === playerTwo ? 1 : 0);
-    data[existingPlayerTwoIndex] = existingPlayerTwoResult;
-  } else {
-    data.push({
-      name: playerTwo,
-      points: wonBy === playerTwo ? 1 : 0,
-    });
-  }
   writeData(filePath, {
-    leaderboard: data,
+    leaderboard: dataToSave,
   });
 };
 
